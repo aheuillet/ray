3d854e999a7aa9e40e6daae305ef7ae594685811
Alexandre Heuillet <ale.heuillet@gmail.com>
2021-05-07 12:24:38 +0200

    started to implement Smothed Epsilon Greedy exploration

--- rllib/contrib/bandits/exploration.py
+++ rllib/contrib/bandits/exploration.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from ray.rllib.models.action_dist import ActionDistribution
 from ray.rllib.utils.annotations import override
-from ray.rllib.utils.exploration.exploration import Exploration
+from ray.rllib.utils.exploration.exploration import Exploration, EpsilonGreedy
 from ray.rllib.utils.framework import TensorType
 
 
@@ -44,3 +44,19 @@ class UCB(Exploration):
         else:
             scores = self.model.value_function()
             return scores.argmax(dim=1), None
+
+class SmoothedEpsilonGreedy(EpsilonGreedy):
+    @override(EpsilonGreedy)
+    def get_exploration_action(self,
+                               action_distribution: ActionDistribution,
+                               timestep: Union[int, TensorType],
+                               explore: bool = True):
+        if self.framework == "torch":
+            return self._get_torch_exploration_action(action_distribution,
+                                                      explore)
+        else:
+            raise NotImplementedError
+    
+    @override(EpsilonGreedy)
+    def _get_torch_exploration_action(self, action_dist, explore):
+        pass #TODO
